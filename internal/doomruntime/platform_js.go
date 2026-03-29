68a6c356b2679268a92e133fc07d3d42027a17f0
Distortions81 <carlotto81@gmail.com>
2026-03-29 02:19:10 -0600

    remove frame cap

--- internal/doomruntime/platform_js.go
+++ internal/doomruntime/platform_js.go
@@ -8,28 +8,11 @@ import (
 	"gddoom/internal/platformcfg"
 )
 
-var lastWASMFrameStart time.Time
-
 func isWASMBuild() bool {
 	return platformcfg.IsWASMBuild()
 }
 
-func yieldWASMRenderTime(frameStart time.Time) {
-	const frameBudget = 15 * time.Millisecond
+func yieldWASMRenderTime(time.Time) {
 	const minYield = 1 * time.Millisecond
-	sleep := minYield
-	if !lastWASMFrameStart.IsZero() {
-		elapsed := frameStart.Sub(lastWASMFrameStart)
-		if elapsed < frameBudget {
-			sleep = frameBudget - elapsed
-			if sleep < minYield {
-				sleep = minYield
-			}
-		}
-	}
-	if sleep < minYield {
-		sleep = minYield
-	}
-	lastWASMFrameStart = frameStart
-	time.Sleep(sleep)
+	time.Sleep(minYield)
 }
