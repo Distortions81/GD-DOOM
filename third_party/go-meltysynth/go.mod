c9ea02369c8d61b342ac37a82e9716ffd75ba183
Distortions81 <carlotto81@gmail.com>
2026-04-03 02:12:30 -0600

    .

--- third_party/go-meltysynth/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/sinshu/go-meltysynth
-
-go 1.19
--- third_party/go-meltysynth/meltysynth/loop_mode.go
+++
@@ -1,7 +0,0 @@
-package meltysynth
-
-const (
-	loop_NoLoop           int32 = 0
-	loop_Continuous       int32 = 1
-	loop_LoopUntilNoteOff       = 3
-)
